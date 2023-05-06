d0961d09a55e016f2fe4010d02be25e2be30ae3a
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-05-06 18:10:06 +0700

    first challange - variable

---
+++ dart_1_variable/bin/dart_1_variable.dart
@@ -0,0 +1,19 @@
+import 'package:dart_1_variable/dart_1_variable.dart' as dart_1_variable;
+
+void main(List<String> arguments) {
+  /*
+    Buatlah sebuah program Dart yang
+    menghitung luas persegi panjang. Program ini
+    harus menggunakan variabel untuk
+    menyimpan nilai panjang dan lebar, dan
+    kemudian mencetak luasnya.
+  */
+
+  int panjang = 20;
+  int lebar = 10;
+  int hasil;
+
+  hasil = panjang * lebar;
+
+  print('luas persegi panjang dengan panjang: ${panjang}cm & lebar: ${lebar}cm adalah: ${hasil}cm2');
+}
---
+++ dart_1_variable/test/dart_1_variable_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_1_variable/dart_1_variable.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
